2194adf5c2834f31a3dfbc403445a07b108eb6f5
Mykhailo Shyhin <johnglagol@gmail.com>
2022-08-13 15:50:08 +0300

    add main

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import Feedback from './Feedback/Feedback';
+import Main from './utils/app.styled';
+
+export const App = () => {
+  return (
+    <Main>
+      <Feedback />
+    </Main>
+  );
+};
---
+++ src/components/Feedback/Notification/Notification.styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+const Warning = styled.p`
+  color: red;
+`;
+
+export default Warning;
